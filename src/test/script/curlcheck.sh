03ac110f51f703b51cb833d8794295d70a7dca23
aoki <aokinobu@gmail.com>
2016-12-14 10:57:11 +0900

    for dev scripts

--- src/test/script/curlcheck.sh
+++ src/test/script/curlcheck.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #curl --header "content-type: text/xml" -d @$1 http://localhost:1010/ws
-curl --header "content-type: text/xml" -d @$1 http://localhost:8031/ws
+#curl --header "content-type: text/xml" -d @$1 http://localhost:8031/ws
+curl --header "content-type: text/xml" -d @$1 http://localhost:891/admin/ws
+#curl --header "content-type: text/xml" -d @$1 http://test.api.glytoucan.org/admin/ws
